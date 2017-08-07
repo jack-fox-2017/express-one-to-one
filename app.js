d6ceb6b3242023864ab951f393aa65f1f554bbd9
Rahmat H <kickyou44@gmail.com>
2017-08-07 11:30:46 +0700

    udah

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express()
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(__dirname + '/public'));
+
+//routing
+let index = require('./routes/index')
+let contacts = require('./routes/contacts')
+let profiles = require('./routes/profiles')
+
+app.use('/', index)
+app.use('/contacts', contacts)
+app.use('/profiles', profiles)
+
+app.listen(3000, () => {
+  console.log("Listenin on port 3000");
+})
---
+++ routes/profiles.js
@@ -0,0 +1,93 @@
+const express = require('express')
+let router = express.Router()
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('./db/data.db')
+
+router.get('/', (req, res) => {
+  db.all(`
+    SELECT
+      profiles.*,
+      contacts.name as contact_name
+      
+      FROM profiles 
+        LEFT JOIN contacts 
+          ON contacts.id=profiles.contact_id
+  `, (errP, rowsP) => {
+    if (errP) throw errP
+      db.all(`
+        SELECT 
+          contacts.id,
+          contacts.name
+
+          FROM contacts 
+            LEFT JOIN profiles 
+              ON contacts.id=profiles.contact_id 
+                WHERE profiles.contact_id is null
+      `, (errC, rowsC) => {
+        if (errC) throw errC
+        res.render('profiles', {
+          data: rowsP,
+          contacts: rowsC
+        })
+      })
+  })
+})
+
+router.post('/', (req, res) => {
+  db.run(`INSERT INTO profiles (username, password, contact_id) VALUES (
+    '${req.body.username}',
+    '${req.body.password}',
+    ${req.body.contact_id == '' ? null : req.body.contact_id}
+  )`, function(err) {
+    if (err) throw err
+    res.redirect('/profiles')
+  })
+})
+
+router.get('/edit/:id', (req, res) => {
+  db.all(`
+    SELECT
+      profiles.*,
+      contacts.name as contact_name
+      
+      FROM profiles 
+        LEFT JOIN contacts 
+          ON contacts.id = profiles.contact_id
+            WHERE profiles.id = ${req.params.id}
+  `, (errP, rowsP) => {
+    if (errP) throw errP
+      db.all(`
+        SELECT 
+          contacts.id,
+          contacts.name
+
+          FROM contacts 
+            LEFT JOIN profiles 
+              ON contacts.id = profiles.contact_id
+                WHERE profiles.contact_id = ${rowsP[0].contact_id} OR profiles.contact_id is null
+      `, (errC, rowsC) => {
+        if (errC) throw errC
+        res.render('profiles-edit', {
+          data: rowsP,
+          contacts: rowsC
+        })
+      })
+  })
+})
+
+router.post('/edit/:id', (req, res) => {
+  db.run(`UPDATE profiles SET
+    username = '${req.body.username}',
+    password = '${req.body.password}',
+    contact_id = ${req.body.contact_id == '' ? null : req.body.contact_id}
+  WHERE id = ${req.params.id}`)
+
+  res.redirect('/profiles')
+})
+
+router.get('/delete/:id', (req, res) => {
+  db.run(`DELETE FROM profiles WHERE id = ${req.params.id}`)
+  res.redirect('/profiles')
+})
+
+module.exports = router
